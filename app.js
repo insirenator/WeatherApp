a8f17e6d916b616d8c677ea89b7cc2e8017f5575
insirenator <arsshakeeb149@protonmail.com>
2023-04-03 11:48:50 +0530

    Added: Search city functionality

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const apiKey = '9d51dd32a009464c83d34112230304';
 
+//Search Bar
+const search = document.querySelector('#search');
+const searchBtn = document.querySelector('#search-btn');
+
+searchBtn.addEventListener('click', () => {
+	const cityName = search.value;
+
+	if(cityName) {
+		const weatherData = getWeatherData(cityName);
+
+		weatherData.then(obj => {
+			console.log(obj.data);
+			displayData(obj.data);
+		}).catch(error => console.log('Error Caught : ', {error}));
+	}
+});
+
+
+// Fields
+const temperature = document.querySelector('#temperature');
+const city = document.querySelector('#city');
+const humidity = document.querySelector('#humidity');
+const precipitation = document.querySelector('#precipitation');
+const wind = document.querySelector('#wind');
+
 // Takes a city the returns the weather data as a promise
 async function getWeatherData(city) {
 	const response = axios.get(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
@@ -7,11 +32,19 @@ async function getWeatherData(city) {
 	return response;
 }
 
-// const weatherIcon = document.querySelector('#weather-icon');
+function displayData(data) {
+	createIcon(data.current.condition.icon);
+	temperature.innerHTML = `${data.current.temp_c}&#x2103;`;
+	city.textContent = `${data.location.name}, ${data.location.country}`;
+	humidity.textContent = `${data.current.humidity}`;
+	precipitation.textContent = `${data.current.precip_in} inches`;
+	wind.textContent = `${data.current.wind_kph} kph`;
+}
 
-
-
-/*weatherData.then(d => {
-	console.log(d.data)
-	weatherIcon.src = d.data.current.condition.icon;
-});*/
+function createIcon(iconLink) {
+	const quickPanel = document.querySelector('.quick-panel');
+	quickPanel.removeChild(quickPanel.lastChild)
+	const icon = document.createElement('img');
+	icon.src = iconLink;
+	quickPanel.appendChild(icon);
+}
